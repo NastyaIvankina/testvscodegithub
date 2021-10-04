2a24a76600e4d0d0dd004b5124c3e259cb623516
nastya <nastyai137@gmail.com>
2021-10-04 10:55:02 +0300

    save state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
+import { filter } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs';
+
+  ngOnInit() {
+    const numbers$ = interval(1000).pipe(take(5));
+    const letters$ = of('a', 'b', 'c', 'd', 'e');
+
+    letters$.pipe(
+      switchMap(x =>
+        numbers$
+          .pipe(take(5))
+          .pipe(map(i => i + x))
+      )
+    ).subscribe(x => console.log(x));
+  }
+
+  ngOnDestroy() {
+
+  }
 }
